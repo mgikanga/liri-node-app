cbcaa852c180186e42f0b004123b2f5950c5b5b2
Monicah Gikanga <mgikanga@Admins-MacBook-Air.local>
2018-05-02 16:19:38 -0700

    added spotify nmp and request code for spotify

--- liri.js
+++ liri.js
@@ -1,4 +1,29 @@
 console.log("i'm here");
 require("dotenv").config();
+var keys = require("./keys.js")
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api')
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+//function for twitter
+ function myTweets(){
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    console.log(JSON.stringify(tweets,null,0))
+  if (!error) {
+    console.log(tweets);
+  }
+  console.log(error);
+});
+ };
+ myTweets();
+
+ //function that for spotify 
+ spotify.search({ type: 'track', query: 'All the Small Things' })
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
